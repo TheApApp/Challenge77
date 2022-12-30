79b93ae14d999763f862860f933f8dd9b223e410
Michael Rowe <support@theapapp.com>
2022-12-29 19:38:41 -0500

    Initial Commit

---
+++ Challenge77/Challenge77App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge77App.swift
+//  Challenge77
+//
+//  Created by Michael Rowe on 12/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Challenge77App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
